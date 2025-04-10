a918907bd9c7d0c4e609073326798df155cef3fe
João Gustavo Soares Bispo <jgbispo20@gmail.com>
2025-04-10 18:30:51 -0300

    fix: correct filename handling in S3 upload to ensure unique naming

--- middleware/uploadS3.js
+++ middleware/uploadS3.js
@@ -16,9 +16,17 @@ const uploadToS3 = (req, res, next) => {
   if (!req.file)
     return res.status(400).json({ error: 'Nenhum arquivo enviado.' });
 
+  req.file.originalname = req.file.originalname.replace(/\s/g, '_');
+  req.file.originalname = req.file.originalname.replace(
+    /[^a-zA-Z0-9_.-]/g,
+    '_'
+  );
+  req.file.originalname = req.file.originalname.replace(/_/g, '-');
+  req.file.originalname += `-${Date.now()}`;
+
   const params = {
     Bucket: process.env.AWS_BUCKET_NAME,
-    Key: req.file.originalname + Date.now(),
+    Key: req.file.originalname,
     Body: req.file.buffer,
     ContentType: req.file.mimetype,
   };
